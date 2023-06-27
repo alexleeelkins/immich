80375d1c5b12cf6e917ef5b5ac4587d44c7e1301
Alex Elkins <alex@alexelkins.xyz>
2023-06-27 13:55:24 -0400

    remove unnecessary 'this.' qualifier

--- mobile/lib/utils/capitalize.dart
+++ mobile/lib/utils/capitalize.dart
@@ -1,7 +1,6 @@
 extension StringExtension on String {
   String capitalize() {
-    return this
-        .split(" ")
+    return split(" ")
         .map((str) =>
             str.isEmpty ? str : str[0].toUpperCase() + str.substring(1))
         .join(" ");
